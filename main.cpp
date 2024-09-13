df586d15b58d81ec1f2d548e0a6f4a3ee0607e38
Stan Davies <stan07davies@gmail.com>
2024-09-13 16:30:19 +0100

    actually traced some rays, checking if they pass through a circle, and then rendering that

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 #include "colour.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& centre, float radius, const ray& r) {
+    vec3 origin_to_circle = centre - r.origin();
+    float a = dot(r.direction(), r.direction());
+    float b = -2.f * dot(r.direction(), origin_to_circle);
+    float c = dot(origin_to_circle, origin_to_circle) - (radius*radius);
+    float discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
 colour ray_colour(const ray& r) {
+    // return red if circle, otherwise don't!!
+    // the centre of this circle is x,y centred on screen, and 1 behind the camera, i.e. on the viewport
+    if (hit_sphere(point3(0.f, 0.f, -1.f), 0.5f, r)) {
+        return colour(1.f, 0.f, 0.f);
+    }
+
     vec3 unit_direction = unit(r.direction());
     // normalised y is such that -1 < y < 1 so we add one and half it to yield a value of a such that 0 < a < 1 which we use for lerping
     float a = 0.5f * (unit_direction.y() + 1.f);
@@ -25,7 +40,7 @@ int main() {
     const float focal_length    = 1.f;
     const float viewport_height = 2.f;
     const float viewport_width  = viewport_height * (float(image_width)/image_height);
-    // we are using left handed coordinates (online thing says right handed but I dispute this)
+    // we are using right handed coordinates
     const point3 camera_centre  = { 0, 0, 0 };
 
     // vectors across the horizontal and down the vertical viewport edges
